1f8f655e2776b10c606c2867ecc2d225133c22c2
Edgar <hi.ed@hotmail.com>
2018-05-05 11:35:13 -0500

    react-router configurado correctamente

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import AppRoutes from './routes';
+
+
 import './index.css';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <Router>
+    <AppRoutes />
+  </Router>,
+  document.getElementById('root')
+);
 registerServiceWorker();
